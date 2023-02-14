24d2e77794a5160fae87372b331e521ea9affe4c
«Apolon» <hertek1990@gmail.com>
2023-02-14 22:13:53 +0700

    Zadacha 2

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    System.Console.WriteLine("MAX" + number1);
+}
+else 
+{
+    System.Console.WriteLine("MAX" + number2);
+}
+if (number1 < number2)
+{
+    System.Console.WriteLine("MIN" + number1);
+}
+else 
+{
+    System.Console.WriteLine("MIN" + number2);
+}
